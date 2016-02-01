43cb2636585ec5e919f9a04b92771bf4ceb23057
Tobias Gradl <tobias.gradl@uni-bamberg.de>
2016-02-01 10:19:18 +0100

    16: Implement POST-Redirect-GET pattern for collection editor
    Task-Url: http://github.com/tgradl/colreg/issues/issue/16

--- src/main/java/eu/dariah/de/colreg/controller/CollectionController.java
+++ src/main/java/eu/dariah/de/colreg/controller/CollectionController.java
@@ -3,7 +3,9 @@ package eu.dariah.de.colreg.controller;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import java.util.Locale;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import eu.dariah.de.colreg.model.Collection;
 import eu.dariah.de.colreg.service.CollectionService;
@@ -30,9 +34,13 @@ public class CollectionController {
 	}
 	
 	@RequestMapping(value="{id}", method=RequestMethod.GET)
-	public String editCollection(@PathVariable String id, Model model, Locale locale) {
+	public String editCollection(@PathVariable String id, Model model, Locale locale, HttpServletRequest request) {
+		Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
+		
 		Collection c;
-		if (id.toLowerCase().equals("new")) {
+		if (inputFlashMap!=null && inputFlashMap.containsKey("c")) {
+			c = (Collection)inputFlashMap.get("c");
+		} else if (id.toLowerCase().equals("new")) {
 			c = collectionService.createCollection();
 		} else {
 			c = collectionService.findCurrentByCollectionId(id);
@@ -45,12 +53,13 @@ public class CollectionController {
 	
 	// TODO Implement as POST-REDIRECT-GET Pattern
 	@RequestMapping(value="{id}", method=RequestMethod.POST)
-	public String saveCollection(@Valid Collection c, Model model, Locale locale) {
+	public String saveCollection(@Valid Collection c, Model model, Locale locale, final RedirectAttributes redirectAttributes) {
 		collectionService.save(c);
 		
-		model.addAttribute("c", c);
+		// Flash attribute only required on error / otherwise load by id in GET
+		redirectAttributes.addFlashAttribute("c", c);
 		
-		return "collection/edit";
+		return "redirect:/collections/" + c.getCollectionId();
 	}
 	
 	@RequestMapping(method=GET, value={"/includes/editAccess"})
