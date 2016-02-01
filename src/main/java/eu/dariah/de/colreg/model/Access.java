86bbd0ed19f46024fc32e183c36eb607fcee7778
Tobias Gradl <tobias.gradl@uni-bamberg.de>
2016-02-01 13:15:24 +0100

    8: Refactor inconsistencies in data model
    Task-Url: http://github.com/tgradl/colreg/issues/issue/8

--- src/main/java/eu/dariah/de/colreg/model/Access.java
+++ src/main/java/eu/dariah/de/colreg/model/Access.java
@@ -1,11 +1,13 @@
 package eu.dariah.de.colreg.model;
 
+import java.util.List;
+
 import eu.dariah.de.colreg.model.vocabulary.EncodingScheme;
 
 public class Access {
 	private String type;
 	private String uri;
-	private EncodingScheme scheme;
+	private List<EncodingScheme> schemes;
 	
 	
 	public String getType() { return type; }
@@ -14,6 +16,6 @@ public class Access {
 	public String getUri() { return uri; }
 	public void setUri(String uri) { this.uri = uri; }
 	
-	public EncodingScheme getScheme() { return scheme; }
-	public void setScheme(EncodingScheme scheme) { this.scheme = scheme; }
+	public List<EncodingScheme> getSchemes() { return schemes; }
+	public void setSchemes(List<EncodingScheme> schemes) { this.schemes = schemes; }
 }
