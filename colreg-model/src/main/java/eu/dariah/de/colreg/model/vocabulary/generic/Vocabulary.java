ed9ae268360877e9827a0aed762ddddb35892058
Tobias Gradl <tobias@tgradl.de>
2018-04-09 15:25:29 +0200

    890: kontrolliertes Vokabular Kollektiontyp
    Task-Url: https://pm.winseda.de/issues/890

--- colreg-model/src/main/java/eu/dariah/de/colreg/model/vocabulary/generic/Vocabulary.java
+++ colreg-model/src/main/java/eu/dariah/de/colreg/model/vocabulary/generic/Vocabulary.java
@@ -5,12 +5,22 @@ import de.unibamberg.minf.dme.model.base.BaseIdentifiable;
 public class Vocabulary extends BaseIdentifiable {
 	private static final long serialVersionUID = -6245008051654334916L;
 	
+	public static final String MESSAGE_CODE_PREFIX = "~eu.dariah.de.colreg.vocabularies.";
+	
 	private String identifier;
-	private String name;
+	private String defaultName;
 	
 	public String getIdentifier() { return identifier; }
 	public void setIdentifier(String identifier) { this.identifier = identifier; }
 	
-	public String getName() { return name; }
-	public void setName(String name) { this.name = name; }
+	public String getDefaultName() { return defaultName; }
+	public void setDefaultName(String defaultName) { this.defaultName = defaultName; }
+	
+	
+	public String getMessageCode() {
+		if (this.identifier==null || this.identifier.trim().isEmpty()) {
+			return null;
+		}
+		return MESSAGE_CODE_PREFIX + this.identifier;
+	}
 }
