ed08620a579d5cbb4f7fc60ef706bac55eecb45b
Tobias Gradl <tobias@tgradl.de>
2018-04-20 15:57:11 +0200

    1062: Verbindungen zwischen Sammlungen unabhängig von Hierarchie zulassen
    Task-Url: https://pm.winseda.de/issues/1062

---
+++ colreg-model/src/main/java/eu/dariah/de/colreg/model/CollectionsRelation.java
@@ -0,0 +1,20 @@
+package eu.dariah.de.colreg.model;
+
+public class CollectionsRelation {
+	private String sourceEntityId;
+	private String targetEntityId;
+	private boolean bidirectional;
+	private String relationTypeId;
+	
+	public String getSourceEntityId() { return sourceEntityId; }
+	public void setSourceEntityId(String sourceEntityId) { this.sourceEntityId = sourceEntityId; }
+	
+	public String getTargetEntityId() { return targetEntityId; }
+	public void setTargetEntityId(String targetEntityId) { this.targetEntityId = targetEntityId; }
+	
+	public boolean isBidirectional() { return bidirectional; }
+	public void setBidirectional(boolean bidirectional) { this.bidirectional = bidirectional; }
+	
+	public String getRelationTypeId() { return relationTypeId; }
+	public void setRelationTypeId(String relationTypeId) { this.relationTypeId = relationTypeId; }
+}
